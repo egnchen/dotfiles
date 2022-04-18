d76f89a65fa532e87174a333982e720e42f54270
eugene <1229522864@qq.com>
2022-04-19 04:45:06 +0800

    bugfix, ssh tunnelling should be working now

--- ssh_tunnel/init.sh
+++ ssh_tunnel/init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # setup a ssh-tunnel through remote forwarding
 # add it as a service using systemd
@@ -43,6 +43,7 @@ while getopts 'hput' opt; do
                 echo "Tunnel port should be a number"
                 exit 1
             fi
+	    ;;
         ?)
             echo "Usage: init.sh -h <host> -p <port> -u <user> -t <tunnel_port>"
             ;;
@@ -51,34 +52,45 @@ done
 
 if [ $tunnel_port -eq 0 ]; then
     echo "Tunnel port not specified, random port will be generated"
-    tunnel_port=$(($RANDOM % 5000 + 15000))
+    tunnel_port=$((15000 + $RANDOM % 5000))
+fi
+
+# generate ssh key first
+if [ ! -e ~/.ssh/id_rsa ]; then
+    echo "Generating ssh key"
+    if [ ! -d ~/.ssh ]; then
+        mkdir ~/.ssh
+    fi
+    ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
+fi 
+
+echo "copying ssh id. you might need to enter the password for remote host."
+if ! ssh-copy-id ${remote_user}@${remote_host}; then
+    echo "failed to copy ssh id."
+    exit 1
 fi
 
 echo "Setting up ssh tunnel with ${remote_user}:${remote_host} port ${remote_port}"
 echo "Remote tunnel port is ${tunnel_port}"
 
-echo "Copying ssh id. You might need to enter the password for remote host."
-if ! ssh-copy-id ${remote_user}@${remote_host}; then
-    echo "Failed to copy ssh id."
-    return 1
-fi
-
-
 # try to establish the tunnel
 # this would try to setup the remote port forwarding and open a terminal at the same time
 ssh -R ${tunnel_port}:localhost:22 -o "ExitOnForwardFailure yes" ${remote_user}@${remote_host} "exit"
 
-if ! $?; then
-    echo "Failed to setup remote port forwarding."
-    return 1
+if [ ! $? ]; then
+    echo "Remote host forwarding failed."
+    exit 1
 fi
 
 srvname="ssh_tunnel_${remote_host}"
 filename="/etc/systemd/system/${srvname}.service"
 if [ -e $filename ]; then
-    echo "Couldn't create service ${srvname}: file ${filename} exists."
-    exit 1
+    read -p "${filename} exists. Overwrite?(y/n) " do_overwrite
+    if [ $do_overwrite != 'y' ]; then
+        exit 1
+    fi 
 fi
+
 
 cat <<EOF | sudo tee $filename
 [Unit]
@@ -93,7 +105,7 @@ RestartSec=15s
 WantedBy=multi-user.target
 EOF
 
-if ! $?; then
+if [ ! $? ]; then
     echo "Failed to write systemd service file."
     exit 1
 fi
@@ -103,9 +115,15 @@ sudo systemctl daemon-reload
 sudo systemctl enable $srvname
 sudo systemctl start $srvname
 
-sudo systemctl status $srvname
-if $?; then
+if sudo systemctl status $srvname; then
     echo "Successfully setup ssh tunnel to ${remote_host}."
+    echo "Add this to your ssh config:"
+    cat <<EOF
+Host        your_hostname
+HostName    localhost
+Port        ${tunnel_port}
+ProxyCommand    ssh -p${remote_port} ${remote_user}@${remote_port}
+EOF
 else
     echo "Systemd service failed. Please check the detail with:"
     echo "$ sudo systemctl status $srvname"
